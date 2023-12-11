4a07e54959595f8287b38823a914714f6dd8e4e6
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-11 17:57:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const makeChange = (c) => {
-  // your name here
-};
+    // Ensure c is a positive integer less than 100 and greater than 0
+    if (typeof c !== 'number' || c <= 0 || c >= 100) {
+        alert("Invalid input. Please enter a positive integer less than 100.");
+        return;
+    }
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+    // Initialize coin counts
+    let quarters = 0, dimes = 0, nickels = 0, pennies = 0;
+
+    // Calculate the number of quarters
+    quarters = Math.floor(c / 25);
+    c %= 25;
+
+    // Calculate the number of dimes
+    dimes = Math.floor(c / 10);
+    c %= 10;
+
+    // Calculate the number of nickels
+    nickels = Math.floor(c / 5);
+    c %= 5;
+
+    // The remaining cents are pennies
+    pennies = c;
+
+    // Return the result as an object
+    return { q: quarters, d: dimes, n: nickels, p: pennies };
+};
